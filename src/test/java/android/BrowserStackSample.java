07abf9fb109a5aa9d093773ae8778a2d40639f12
NAnubhav <101396447+NAnubhav@users.noreply.github.com>
2022-03-11 15:40:23 +0530

    Update BrowserStackSample.java

--- src/test/java/android/BrowserStackSample.java
+++ src/test/java/android/BrowserStackSample.java
@@ -1,68 +1,199 @@
-package android;
+package com.qa.baseTest;
 
-import java.net.URL;
-import java.util.List;
-import java.util.function.Function;
-import java.net.MalformedURLException;
-
-import io.appium.java_client.MobileBy;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
-
+import com.aventstack.extentreports.Status;
+import com.qa.reports.ExtentReport;
+import com.qa.utils.TestUtils;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.InteractsWithApps;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.*;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+
+    protected static AppiumDriver driver;
+    protected static Properties prop;
+    private static AppiumDriverLocalService server;
+    InputStream inputstream;
+    TestUtils util = new TestUtils();
+
+    public BaseTest() {
+
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+    @BeforeClass
+    @Parameters({"platformName", "deviceName", "udid"})
+    public void initialSetup(String platformName, String deviceName, String udid) {
+        try {
+
+            util.log().info("Initial set up has been started ...");
+            prop = new Properties();
+            String propFileName = "config.properties";
+            inputstream = getClass().getClassLoader().getResourceAsStream(propFileName);
+            prop.load(inputstream);
+            util.log().info(propFileName + "loaded sucessfully...");
+
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability(MobileCapabilityType.PLATFORM_NAME, platformName);
+            util.log().info("Mobile automation platform used : " + platformName);
+
+            caps.setCapability("newCommandTime", 300);
+            caps.setCapability(MobileCapabilityType.DEVICE_NAME, deviceName);
+            caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, prop.getProperty("androidAutomationName"));
+            caps.setCapability(MobileCapabilityType.UDID, udid);
+            caps.setCapability("avd", "Anubhav_Pixel_1");
+            caps.setCapability("avdLaunchTimeOut", 180000);
+            caps.setCapability("newCommandTimeOUt", 300);
+            String appUrl = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main" +
+                    File.separator + "resources" + File.separator + "app" + File.separator + "app-release.apk";
+            caps.setCapability(MobileCapabilityType.APP, appUrl);
+           // caps.setCapability("appPackage", prop.getProperty("androidAppPackage"));
+          // caps.setCapability("appActivity", prop.getProperty("androidAppActivity"));
+
+            URL url = new URL(prop.getProperty("appiumURL"));
+            driver = new AppiumDriver(url, caps);
+
+            System.out.println("session id :" + driver.getSessionId());
+            if (driver.getSessionId() != null) {
+                util.log().info("Appium driver has been initialized sucessfully");
+                util.log().info("Appirum driver session id is " + driver.getSessionId());
+            }
 
 
-public class BrowserStackSample {
+            driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+            util.log().info("implicitly time out wait set to 20 second");
+        } catch (Exception e) {
+            e.printStackTrace();
+            util.log().fatal("Appium Driver not initialized");
+        }
+    }
 
-	public static void main(String[] args) throws MalformedURLException, InterruptedException {
-		
-    	DesiredCapabilities caps = new DesiredCapabilities();
-    	
-    	// Set your access credentials
-    	caps.setCapability("browserstack.user", "YOUR_USERNAME");
-    	caps.setCapability("browserstack.key", "YOUR_ACCESS_KEY");
-    	
-    	// Set URL of the application under test
-    	caps.setCapability("app", "bs://<app-id>");
-    	
-    	// Specify device and os_version for testing
-    	caps.setCapability("device", "Google Pixel 3");
-    	caps.setCapability("os_version", "9.0");
-        
-    	// Set other BrowserStack capabilities
-    	caps.setCapability("project", "First Java Project");
-    	caps.setCapability("build", "browserstack-build-1");
-    	caps.setCapability("name", "first_test");
-       
-    	
-    	// Initialise the remote Webdriver using BrowserStack remote URL
-    	// and desired capabilities defined above
-        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(
-        		new URL("http://hub.browserstack.com/wd/hub"), caps);
-        
 
-        // Test case for the BrowserStack sample Android app. 
-        // If you have uploaded your app, update the test case here. 
-        AndroidElement searchElement = (AndroidElement) new WebDriverWait(driver, 30).until(
-            ExpectedConditions.elementToBeClickable(
-            		MobileBy.AccessibilityId("Search Wikipedia")));
-        searchElement.click();
-		AndroidElement insertTextElement = (AndroidElement) new WebDriverWait(driver, 30).until(
-             ExpectedConditions.elementToBeClickable(
-            		 MobileBy.id("org.wikipedia.alpha:id/search_src_text")));
-        insertTextElement.sendKeys("BrowserStack");
-        Thread.sleep(5000);
-        List<AndroidElement> allProductsName = driver.findElementsByClassName(
-        		"android.widget.TextView");
-        assert(allProductsName.size() > 0);
-        
-        
-        // Invoke driver.quit() after the test is done to indicate that the test is completed.
+   /* @BeforeSuite
+    public void beforeSuite() {
+
+//        server = getAppiumServerDefault();
+//        server.start();
+//        server.clearOutPutStreams();
+        util.log().info("server started successfully");
+    }*/
+
+    @AfterClass
+
+        public void tearDown(){
+            driver.quit();
+        }
+
+
+    public AppiumDriverLocalService getAppiumServerDefault() {
+        return AppiumDriverLocalService.buildDefaultService();
+    }
+
+
+    public AppiumDriver getDriver() {
+        return driver;
+    }
+
+
+    public void waitForVisibility(MobileElement ele) {
+        WebDriverWait wait = new WebDriverWait(driver, 50);
+        wait.until(ExpectedConditions.visibilityOf(ele));
+    }
+
+    public void clickOnElement(MobileElement ele) {
+        waitForVisibility(ele);
+        ele.click();
+    }
+
+    public boolean clickOnElement(MobileElement ele, String msg) {
+        try {
+            waitForVisibility(ele);
+            ele.click();
+            util.log().info(msg);
+            ExtentReport.getTest().log(Status.PASS, msg);
+            return true;
+        } catch (Exception e) {
+            util.log().error(msg + "is failed.");
+            ExtentReport.getTest().log(Status.FAIL, msg);
+            return false;
+        }
+
+    }
+
+
+    public void enterTxtOnElement(MobileElement ele,String txt){
+        waitForVisibility(ele);
+        ele.sendKeys(txt);
+    }
+
+    public void enterTxtOnElement(MobileElement ele,String txt,String msg){
+        waitForVisibility(ele);
+
+        ele.click();
+      //  ele.setValue(txt);
+      ele.sendKeys(txt);
+        util.log().info(msg+ ": "+txt);
+        ExtentReport.getTest().log(Status.PASS, msg+ " : "+txt);
+
+
+    }
+
+    public String getText(MobileElement ele){
+        String txt = null;
+        waitForVisibility(ele);
+        txt = ele.getAttribute("text");
+        return txt;
+    }
+
+    public String getText(MobileElement ele, String msg) {
+        String txt = null;
+        waitForVisibility(ele);
+        txt = ele.getAttribute("text");
+        if(txt!=null){
+            util.log().info(msg + txt);
+            ExtentReport.getTest().log(Status.PASS, msg +"  "+ txt); }
+         else{
+            util.log().info(msg + txt);
+            ExtentReport.getTest().log(Status.FAIL, msg +"  "+ txt);
+        }
+        return txt;
+
+        }
+
+
+
+
+    public void closeApp() {
+        ((InteractsWithApps) getDriver()).closeApp();
+    }
+
+    public void launchApp() {
+        ((InteractsWithApps) getDriver()).launchApp();
+    }
+
+
+   /* @AfterTest()
+   // public void tearDown(){
         driver.quit();
-		
-	}
+    }
+*/
+
 
 }
